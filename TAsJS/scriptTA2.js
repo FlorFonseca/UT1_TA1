abfe9f59d930c0c58f92d7636010d13baf1f3883
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2024-08-14 23:47:23 -0300

    ta16 terminado

--- TAsJS/scriptTA2.js
+++
@@ -1,6 +0,0 @@
-function removeFromArray ( arreglo, item){
-    arreglo=arreglo.filter((element)=>element!==item);
-    console.log(arreglo);
-}
-
-removeFromArray(["hola","chau","buenas"], "chau");
